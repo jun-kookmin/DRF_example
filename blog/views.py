9b3c028040e7c8b8d46170380f6a3553c23d7be0
xodls <pider6@naver.com>
2025-05-23 16:27:04 +0900

    permissions를 view와 연결해줌, 포스트뷰셋과 커멘트 뷰셋에 권한 있어야 수정/삭제 가능하게함

--- blog/views.py
+++ blog/views.py
@@ -2,11 +2,12 @@ from rest_framework import viewsets, permissions
 from .models import Post, Comment
 from django.contrib.auth.models import User
 from .serializers import PostSerializer, CommentSerializer, UserSerializer
+from .permissions import IsAuthorOrReadOnly
 
 class PostViewSet(viewsets.ModelViewSet):
     queryset = Post.objects.all().order_by('-created_at')
     serializer_class = PostSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -14,7 +15,7 @@ class PostViewSet(viewsets.ModelViewSet):
 class CommentViewSet(viewsets.ModelViewSet):
     queryset = Comment.objects.all().order_by('-created_at')
     serializer_class = CommentSerializer
-    permission_classes = [permissions.IsAuthenticatedOrReadOnly]
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly, IsAuthorOrReadOnly]
 
     def perform_create(self, serializer):
         serializer.save(author=self.request.user)
@@ -22,4 +23,4 @@ class CommentViewSet(viewsets.ModelViewSet):
 class UserViewSet(viewsets.ReadOnlyModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    
+
